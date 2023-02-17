931f009c3698f97e06a965b6f67cde87c1f3eaa0
Aleksandar Milic <alexmilic01@gmail.com>
2023-02-17 21:15:53 +0100

    Init setup commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <header>
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <div>
+    <RouterView />
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { RouterLink, RouterView } from 'vue-router';
+
+</script>
